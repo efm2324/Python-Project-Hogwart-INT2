92762e56c991789c38c8af36e2b95c324c8aa84e
efm2324 <gonie.louis@gmail.com>
2025-11-28 23:22:23 +0100

    Import additional input utility functions

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-
 from utils.input_utils import ask_text
-
-def main():
-    name = ask_text("Please enter your name: ")
-    print(f"Hello, {name}!")
-
-if __name__ == "__main__":
-    main()
+from utils.input_utils import ask_number
+from utils.input_utils import ask_choice
