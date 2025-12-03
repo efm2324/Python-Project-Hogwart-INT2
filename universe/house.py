4f5be388f2109d631da9c64e70210b0a57463d79
Akitsuuuuu <kenny.banh.ly@gmail.com>
2025-12-03 16:52:13 +0100

    .

--- universe/house.py
+++ universe/house.py
@@ -1,1 +1,25 @@
 
+
+houses = {
+    "Gryffindor": 0,
+    "Slytherin": 0,
+    "Hufflepuff": 0,
+    "Ravenclaw": 0
+}
+
+def update_house_points(houses, house_name, points):
+    if house_name in houses:
+        houses[house_name] += points
+        return houses
+    else :
+        print("House not found.")
+        return None
+
+def display_winning_house(houses):
+    max_points = max(houses.values())
+    winning_houses = [house for house, points in houses.items() if points == max_points]
+    
+    if len(winning_houses) > 1:
+        print(f"It's a tie! {', '.join(winning_houses)} are tied with {max_points} points!")
+    else:
+        print(f"The winning house is {winning_houses[0]} with {max_points} points!")
